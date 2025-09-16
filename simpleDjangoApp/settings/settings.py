431d66b45b0f78522e3334fdb21a25eca097e190
Magzhan Dautbekov <magzhandautbekov@gmail.com>
2025-09-16 14:31:29 +0500

    small updates

--- simpleDjangoApp/settings/settings.py
+++ simpleDjangoApp/settings/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9+wo#k*j$#r^!01mj5y&m23pbv%w()u^8ws8%%5mqus-@n$exe'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
